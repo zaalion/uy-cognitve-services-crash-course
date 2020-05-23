a78b3f2ae1815de7c01bd86915915cd59cfa6cae
Reza Salehi <zaalion@gmail.com>
2020-05-23 15:39:41 -0400

    Initial Checkin

---
+++ Azure_Cognitive_Services_Crash_Course/02-Demos-Resources/VS_CSharp/03-Language/ImmReaderApp-Core/ImmReaderApp-Core/Controllers/HomeController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ImmReaderApp_Core.Models;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace ImmReaderApp_Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string TenantId;     // Azure subscription TenantId
+        private readonly string ClientId;     // Azure AD ApplicationId
+        private readonly string ClientSecret; // Azure AD Application Service Principal password
+        private readonly string Subdomain;
+
+        public HomeController(Microsoft.Extensions.Configuration.IConfiguration configuration)
+        {
+            TenantId = configuration["TenantId"];
+            ClientId = configuration["ClientId"];
+            ClientSecret = configuration["ClientSecret"];
+            Subdomain = configuration["Subdomain"];
+
+            if (string.IsNullOrWhiteSpace(TenantId))
+            {
+                throw new ArgumentNullException("TenantId is null! Did you add that info to secrets.json?");
+            }
+
+            if (string.IsNullOrWhiteSpace(ClientId))
+            {
+                throw new ArgumentNullException("ClientId is null! Did you add that info to secrets.json?");
+            }
+
+            if (string.IsNullOrWhiteSpace(ClientSecret))
+            {
+                throw new ArgumentNullException("ClientSecret is null! Did you add that info to secrets.json?");
+            }
+
+            if (string.IsNullOrWhiteSpace(Subdomain))
+            {
+                throw new ArgumentNullException("Subdomain is null! Did you add that info to secrets.json?");
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetTokenAndSubdomain()
+        {
+            try
+            {
+                string tokenResult = await GetTokenAsync();
+
+                return new JsonResult(new
+                {
+                    token = tokenResult,
+                    subdomain = Subdomain
+                });
+            }
+            catch (Exception e)
+            {
+                string message = "Unable to acquire Azure AD token. Check the debugger for more information.";
+                Debug.WriteLine(message, e);
+                return new JsonResult(new { error = message });
+            }
+        }
+
+        /// <summary>
+        /// Get an Azure AD authentication token
+        /// </summary>
+        private async Task<string> GetTokenAsync()
+        {
+            string authority = $"https://login.windows.net/{TenantId}";
+            const string resource = "https://cognitiveservices.azure.com/";
+
+            AuthenticationContext authContext = new AuthenticationContext(authority);
+            ClientCredential clientCredential = new ClientCredential(ClientId, ClientSecret);
+
+            AuthenticationResult authResult = await authContext.AcquireTokenAsync(resource, clientCredential);
+
+            return authResult.AccessToken;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # uy-cognitve-services-crash-course
+
+Course URL: https://www.udemy.com/course/azure-cognitive-services-crash-course
